82ac2339e2c9ffcb82ab5783469d15d1da42e774
adisrael <adisrael@uc.cl>
2018-04-20 15:43:14 -0300

    welcome-page: added username validation in user model, added not functioning dropdown menu in welcome-view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+	validates :userName, 
+          :presence => {:message => "Username can't be blank." },
+          :uniqueness => {:message => "Username already exists."}
+	# validates :password, presence: true
 end
