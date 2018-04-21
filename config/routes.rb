5f40ab8237225372f8ff4b4740fc7b92e8d71105
Alfredo De Goyeneche <asdegoyeneche@uc.cl>
2018-04-21 18:33:39 -0300

    fixes a usuarios y otros

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
   resources :comments
   resources :subscriptions
   resources :forums
-  #resources :users
+  resources :users
   get 'welcome/index'
   root 'welcome#index'
+  #get '/users/:id', :to => 'users#show', :as => :user
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20180418200443_create_comments.rb
+++ db/migrate/20180418200443_create_comments.rb
@@ -4,7 +4,7 @@ class CreateComments < ActiveRecord::Migration[5.1]
       t.string :content
       t.references :user, foreign_key: true
       t.references :publication, foreign_key: true
-
+      
       t.timestamps
     end
   end
