8e055ee71d4800009a4a6cf3c00183fb83884aad
Yoav Navon <yanavon@uc.cl>
2018-04-21 19:58:07 -0300

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'proyecto-software-grupo29.herokuapp.com' }
 end
