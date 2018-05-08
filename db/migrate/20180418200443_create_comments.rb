56ebca8f54629b6ce184086abb75fa0d409d6ad9
Yoav Navon <yanavon@uc.cl>
2018-05-08 16:35:26 -0300

    added vote attribute on publication, comment

--- db/migrate/20180418200443_create_comments.rb
+++ db/migrate/20180418200443_create_comments.rb
@@ -2,9 +2,9 @@ class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
       t.string :content
+      t.integer :votes
       t.references :user, foreign_key: true
       t.references :publication, foreign_key: true
-      
       t.timestamps
     end
   end
