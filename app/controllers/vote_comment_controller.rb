4b4a0b6914c330cda6e9df9f5d2e1d9cc17bca37
Yoav Navon <yanavon@uc.cl>
2018-05-09 15:49:49 -0300

    created create controller vote comment

--- app/controllers/vote_comment_controller.rb
+++ app/controllers/vote_comment_controller.rb
@@ -1,4 +1,33 @@
 class VoteCommentController < ApplicationController
   def create
+    data = vote_params #  user_id, comment_id
+    result = VoteComment.where(user_id: data[:user_id],\
+                               comment_id: data[:comment_id]).to_a
+    comment = Comment.find(data[:comment_id])
+    respond_to do |format|
+      if result.empty?
+        @vote_comment = VoteComment.new(data)
+        comment.votos += 1
+        if @vote_comment.save && comment.save
+          format.html do
+            redirect_to publication, notice: 'UpVoted.'
+          end
+        else
+          format.html do
+            redirect_to publication, notice: 'Error ocurred'
+          end
+        end
+      else
+        format.html do
+          redirect_to publication, notice: 'Already voted this commentary'
+        end
+      end
+    end
+  end
+
+  private
+
+  def vote_params
+    params.require(:vote).permit(:user_id, :comment_id)
   end
 end
