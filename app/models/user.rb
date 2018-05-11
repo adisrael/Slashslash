01bfe1f6cd94a288219917100366fb30bb693bdb
adisrael <adisrael@uc.cl>
2018-05-11 17:13:52 -0300

    user-roles: added default role upon creation = 2 (normal user)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ApplicationRecord
   has_many :subscriptions, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :vote_comments, dependent: :destroy
+
+  before_create :default_role
+  def default_role
+    self.role ||= 2
+  end
 end
