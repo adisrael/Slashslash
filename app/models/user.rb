55fb83256ea6b141b3374b5dbb3d28b81c162584
Yoav Navon <yanavon@uc.cl>
2018-05-11 20:46:19 -0300

    model asosiation for moderator

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :subscriptions, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :vote_comments, dependent: :destroy
+  has_many :modered_forums, through: :moderators, source: :forums
 
   before_create :default_role
   def default_role
