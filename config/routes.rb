e530bc6b26c9d5303261ad408001806310cac495
Yoav Navon <yanavon@uc.cl>
2018-05-24 17:13:16 -0400

    organized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,43 @@
 Rails.application.routes.draw do
+
+  # devise
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'registrations'
+  }
+
+  # vote comments
   post 'vote_comment/create', as: 'vote_comments'
+
+  # vote publications
   post 'vote', as: 'votes', to: 'vote#create'
   delete 'vote', to: 'vote#destroy'
+
+  # moderators
   post 'moderate/request', to: 'requests#apply'
   patch 'moderate/request/:id', to: 'requests#accept'
   delete 'moderate/request/:id', to: 'requests#reject'
   get 'moderate/request', to: 'requests#show', as: 'requests'
   delete 'moderate', to: 'requests#resign'
 
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'registrations'
-  }
-
+  # resources
   resources :publications
   resources :comments
   resources :subscriptions
   resources :forums
+  resources :users
+
+  # forums
   get '/forums/:id/new_publication', to: 'forums#new_publication', as: 'new_forum_publication'
+
+  # search
   get '/home/search/forums', to: 'home#search_forum', as: 'search_forum'
   get '/home/search/publications', to: 'home#search_publication', as: 'search_publication'
   get '/home/search/users', to: 'home#search_user', as: 'search_user'
-  resources :users
+
+  # home and root
   get 'welcome/index'
   root 'welcome#index'
-  # get '/users/:id', :to => 'users#show', :as => :user
-
   get '/home', to: 'home#index', as: :home
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
