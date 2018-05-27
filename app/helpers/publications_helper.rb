1bc339021a48b24d6009739a15aef7c94fc25527
Yoav Navon <yanavon@uc.cl>
2018-05-27 17:33:06 -0400

    user credentials on publications

--- app/helpers/publications_helper.rb
+++ app/helpers/publications_helper.rb
@@ -1,2 +1,10 @@
 module PublicationsHelper
+  def check_credentials(user, publication)
+    if user.role.zero?
+      return true
+    elsif user.moderated_forums.include?(publication.forum)
+      return true
+    end
+    false
+  end
 end
