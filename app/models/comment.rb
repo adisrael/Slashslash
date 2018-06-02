97e15068c0ea4ff461a89610fe3a17c44c236f38
Yoav Navon <yanavon@uc.cl>
2018-06-02 16:08:23 -0400

    comments on cascade destroy

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
   has_many :vote_comments, dependent: :destroy
 end
