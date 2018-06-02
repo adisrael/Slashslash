24a76e7876bd9d117fd654fc2f2c1acc7de96d81
Alfredo De Goyeneche <asdegoyeneche@uc.cl>
2018-06-02 01:20:35 -0400

    Guest user restriction

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+f# frozen_string_literal: true
 
 class PublicationsController < ApplicationController
   before_action :set_publication, only: %i[show edit update destroy]
