cb6687cea63cda4fdc51e482d23eab77c3869ee4
Alfredo De Goyeneche <asdegoyeneche@uc.cl>
2018-06-02 19:58:29 -0400

    final version

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20180602230614) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -98,7 +97,6 @@ ActiveRecord::Schema.define(version: 20180602230614) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
