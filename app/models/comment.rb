1f8459376a7abc763cfc476eb6806f6e998658c6
Yoav Navon <yanavon@uc.cl>
2018-06-02 14:22:49 -0400

    polymorphic assosiations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :publication
+  belongs_to :commentable, polymorphic: true
+  has_many :comments, as: :commentable
   has_many :vote_comments, dependent: :destroy
 end
--- db/migrate/20180418200443_create_comments.rb
+++ db/migrate/20180418200443_create_comments.rb
@@ -3,8 +3,8 @@ class CreateComments < ActiveRecord::Migration[5.1]
     create_table :comments do |t|
       t.string :content
       t.integer :votos
+      t.integer :comentable_id
       t.references :user, foreign_key: true
-      t.references :publication, foreign_key: true
       t.timestamps
     end
   end
