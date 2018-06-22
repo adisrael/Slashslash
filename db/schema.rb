65e981a0d5e9d4d9dd2d13964e1a7da57e92f111
Alfredo De Goyeneche <asdegoyeneche@uc.cl>
2018-06-22 19:09:22 -0400

    modified facebook information retrieval

--- db/schema.rb
+++ db/schema.rb
@@ -130,7 +130,6 @@ ActiveRecord::Schema.define(version: 20180622221123) do
     t.datetime "avatar_updated_at"
     t.string "provider"
     t.string "uid"
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
