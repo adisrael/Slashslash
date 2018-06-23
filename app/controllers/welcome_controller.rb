933bc32870f50955ea630b5db6bbfc37e323755e
Yoav Navon <yanavon@uc.cl>
2018-06-23 13:49:52 -0400

    added devise partial

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,24 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
+  helper_method :resource_name, :resource, :devise_mapping, :resource_class
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_class
+    User
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
   def index
     redirect_to home_path if user_signed_in?
   end
